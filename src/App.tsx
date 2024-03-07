1d75d4c3b76a7fe95a3ca05a08338a4c7e37f0c8
Vlad Lazar <lazar.vlad151@gmail.com>
2024-03-07 23:43:15 +0100

    implement echart with treemap and sunburst

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import ChartSwitcher from "./components/chart/chart";
 import PageHeader from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
       <PageHeader />
+
+      <ChartSwitcher />
     </div>
   );
 }
---
+++ src/components/chart/skillsData.ts
@@ -0,0 +1,62 @@
+const skillsData = [
+  {
+    name: "Frontend",
+    value: 1090,
+    children: [
+      { name: "React.js", value: 200 },
+      { name: "JavaScript", value: 100 },
+      { name: "TypeScript", value: 200 },
+      { name: "Vue.js", value: 30 },
+      { name: "HTML", value: 100 },
+      { name: "SCSS", value: 100 },
+      { name: "yarn", value: 100 },
+      { name: "npm", value: 100 },
+      { name: "CSS", value: 100 },
+      { name: "Webpack", value: 60 },
+    ],
+  },
+  {
+    name: "Backend",
+    value: 500,
+    children: [
+      { name: ".NET", value: 100 },
+      { name: "C#", value: 100 },
+
+      { name: "RESTful API", value: 100 },
+      { name: "SQL", value: 60 },
+      { name: "MongoDB", value: 20 },
+      { name: "Python", value: 30 },
+      { name: "Java", value: 60 },
+      { name: "Spring", value: 30 },
+    ],
+  },
+
+  {
+    name: "Other",
+    value: 960,
+    children: [
+      { name: "Docker", value: 30 },
+      { name: "Azure", value: 30 },
+      { name: "GitLab", value: 50 },
+      { name: "GitHub", value: 50 },
+      { name: "Git", value: 100 },
+      { name: "Communication", value: 100 },
+      { name: "Teamwork", value: 100 },
+      { name: "Problem Solving", value: 100 },
+      { name: "Time Management", value: 100 },
+      { name: "Adaptability", value: 100 },
+      { name: "Creativity", value: 100 },
+      { name: "Critical Thinking", value: 100 },
+    ],
+  },
+  {
+    name: "Language Skills",
+    value: 270,
+    children: [
+      { name: "English", value: 100 },
+      { name: "Romanian", value: 100 },
+      { name: "Russian", value: 70 },
+    ],
+  },
+];
+export default skillsData;
