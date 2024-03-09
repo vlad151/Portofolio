ceebb98989c99af688b5cdb0acf01490169c8df9
Vlad Lazar <lazar.vlad151@gmail.com>
2024-03-09 01:49:49 +0100

    create chatbot ui

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -2,10 +2,12 @@ import { Box } from "@mui/material";
 import ChartSwitcher from "../chart/chart";
 import PageHeader from "../header/header";
 import "./styles.css";
+import ChatUI from "../chatbot/chatUI";
 const Content: React.FC = () => {
   return (
     <Box className="appContent">
       <PageHeader />
+      <ChatUI />
       <ChartSwitcher />
     </Box>
   );
